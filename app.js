d997d085d0135667c64da52ce2b5ea1e03ccab24
Brandon McCulloh <brandonmcculloh@gmail.com>
2020-10-15 23:55:00 -0400

    sequelize changes

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 // Additionals:
-var session = require('express-session');
-var passport = require('passport');
-var sassMiddleware = require('node-sass-middleware');
-var bodyParser = require('body-parser');
-var flash = require('connect-flash');
-var methodOverride = require('method-override');
-var mysql = require('mysql');
+const session = require('express-session');
+const passport = require('passport');
+const sassMiddleware = require('node-sass-middleware');
+const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const methodOverride = require('method-override');
+const cors = require("cors");
 
-var app = express();
+const app = express();
+
+app.use(express.static('public'));
+
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+app.use(cors());
 
 // To allow put and delete methods
 app.use(methodOverride("_method"));
-
-// Support parsing of application/json type post data
-app.use(bodyParser.json());
 
 // Support parsing of application/x-www-form-urlencoded post data
 app.use(
@@ -28,42 +33,65 @@ app.use(
   })
 );
 
+// Support parsing of application/json type post data
+app.use(bodyParser.json());
+
+// for parsing multipart/form-data
+// app.use(upload.array()); 
+// app.use(express.static('public'));
+
+// Cross browsers
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Passport Config
-// require("./config/passport")(passport);
+require("./config/passport")(passport);
 
-// DB Config & conncection point
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'local',
-  password : 'secret',
-  database : 'my_db'
-});
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
 
-  console.log('connected as id ' + connection.threadId);
+// Define server port
+const PORT = process.env.PORT || 8080;;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
 
+
+const db = require("./models");
+// RUN BELOW CODE TO UPDATE TABLES, Alter allows table column modifications directly from models
+db.sequelize.sync({ alter: true });
+db.sequelize.sync({ force: true });
+db.sequelize.query('SET FOREIGN_KEY_CHECKS = 0')
+.then(function(){
+    return db.sequelize.sync({ force: true });
+})
+.then(function(){
+    return db.sequelize.query('SET FOREIGN_KEY_CHECKS = 1');
+})
+.then(function(){
+    console.log('Database synchronised.');
+}, function(err){
+    console.log(err);
+});
 
 // EJS
 app.set("view engine", "ejs");
 
 // Express Session
+// Time for session to expire
 app.use(
   session({
     secret: "secret",
     resave: true,
     saveUninitialized: true,
+    cookie: {maxAge: 10000000}
   })
 );
 
 // Support for flash messages
 app.use(flash());
 app.use(function (req, res, next) {
-
   res.locals.success = req.flash("success");
   res.locals.check = req.flash("check");
   res.locals.failure = req.flash("failure");
@@ -75,39 +103,16 @@ app.use(function (req, res, next) {
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// Routes
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-//var associationsRouter = require("./routes/associations");
-//var managersRouter = require("./routes/managers");
-//var apiRouter = require("./routes/api");
-// Pair Routes with subdirectories
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-//app.use("/associations", associationsRouter);
-//app.use("/managers", managersRouter);
-//app.use("/api", apiRouter);
-
-// Define server port
-const PORT = process.env.PORT;
-// USING `
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
-
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(logger("dev"));
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: false,
-  })
-);
+// app.use(express.json());
+// app.use(
+//   express.urlencoded({
+//     extended: true,
+//   })
+// );
 app.use(cookieParser());
 
 app.use(
@@ -120,20 +125,45 @@ app.use(
 );
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var malRouter = require("./routes/mal");
+var mmlRouter = require("./routes/mml");
+//var associationsRouter = require("./routes/associations");
+//var managersRouter = require("./routes/managers");
+//var apiRouter = require("./routes/api");
+// Pair Routes with subdirectories
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/mal", malRouter);
+app.use("/mml", mmlRouter);
+//app.use("/associations", associationsRouter);
+//app.use("/managers", managersRouter);
+//app.use("/api", apiRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
+// error handler middleware
+app.use((error, req, res, next) => {
+  console.error(error.stack);
+  next(createError(500));
+});
+
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    message: err.message,
+    error: err
+  });
 });
+
+
+
+
 
 module.exports = app;
