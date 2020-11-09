842bec004225edc8b496859007ca92a2b9caff45
Nathan Bianco <nathan.bianco@tamu.edu>
2020-11-09 16:50:26 -0600

    Add mml models to index.js

--- models/index.js
+++ models/index.js
@@ -21,6 +21,7 @@ db.sequelize = sequelize;
 
 db.users = require("./user.model.js")(sequelize, Sequelize);
 db.updates = require("./update.model.js")(sequelize, Sequelize);
+//
 db.mal = require("./mal.model.js")(sequelize, Sequelize);
 db.supplementalAssociationInfo = require("./mal_SupplementalAssociationInfo.model.js")(sequelize, Sequelize);
 db.generalBoardInfo = require("./mal_GeneralBoardInfo.model.js")(sequelize, Sequelize);
@@ -34,6 +35,17 @@ db.referralSource = require("./mal_ReferralSource.model.js")(sequelize, Sequeliz
 db.referralMgmtCoVendor = require("./mal_ReferralMgmtCoVendor.model.js")(sequelize, Sequelize);
 db.presentations = require("./mal_Presentations.model.js")(sequelize, Sequelize);
 db.potentialClientReports = require("./mal_PotentialClientReports.model.js")(sequelize, Sequelize);
+//
+db.mml = require("./mml.model.js")(sequelize, Sequelize);
+db.generalInformation = require("./mml_GeneralInformation.model.js")(sequelize, Sequelize);
+db.businessInformation = require("./mml_BusinessInformation.model.js")(sequelize, Sequelize);
+db.onSiteInformation = require("./mml_OnSiteInformation.model.js")(sequelize, Sequelize);
+db.staffInformation = require("./mml_StaffInformation.model.js")(sequelize, Sequelize);
+db.homeInformation = require("./mml_HomeInformation.model.js")(sequelize, Sequelize);
+db.socialMediaExtras = require("./mml_SocialMediaExtras.model.js")(sequelize, Sequelize);
+db.referrals = require("./mml_Referrals.model.js")(sequelize, Sequelize);
+//
+
 
 
 module.exports = db;
